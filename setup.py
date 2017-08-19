5c83c36170ce78174eb807612eb6d420438244f9
Evan Parker <evanp@google.com>
2017-08-19 01:09:56 -0700

    changed version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='evanptestmodule',
-    version='0.1',
+    version='0.3',
     description='test module',
     author='Evan Parker',
     author_email='',
