93621756d449e73073d6d3a64a9b090e44a939b1
Evan Parker <evanp@google.com>
2017-08-19 00:07:01 -0700

    dummy releaseable module

---
+++ evanptestmodule.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+import json
+
+def run():
+    print(json.dumps({'test': 123}))
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='evanptestmodule',
+    version='0.1',
+    description='test module',
+    author='Evan Parker',
+    author_email='',
+    py_modules=['evanptestmodule'],
+    license='Apache 2.0',
+    use_2to3=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Other/Nonlisted Topic',
+    ]
+)
